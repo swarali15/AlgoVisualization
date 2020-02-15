987ca41f5a055480ab678556604fc7391ce06968
swarali15 <swarali.purandare18@vit.edu>
2020-02-15 06:38:07 +0530

    Array displayed

--- AlgoV.js
+++ AlgoV.js
@@ -1,8 +1,45 @@
-const inputs = document.querySelectorAll('.controls input');
+var color=document.getElementById("arrcolor");
 
-function handleUpdate() {
-   console.log(this.value);
+window.onload=function(){
+    var inputLength = document.getElementById("length");
+var inputLHtag = document.getElementById("ArrayL");
+inputLHtag.innerHTML = inputLength.value;
+
+inputLength.oninput = function() {
+  inputLHtag.innerHTML = this.value;
+    var array=[];
+            //removing prevoius blocks
+            var arrContainer=document.getElementById("ArrayDisplay");
+         var child=arrContainer.lastElementChild;
+         while(child)
+             {
+                 arrContainer.removeChild(child);
+                 child=arrContainer.lastElementChild;
+             }
+    var num=this.value;
+    for(var i=0;i<num;i++)
+        {
+         //Adding new div blocks
+            var ht=Math.floor(Math.random()*100);
+         
+            array.push(ht);
+           var newDiv = document.createElement('div');
+newDiv.style.cssText = 'opacity:0.3;z-index:100; padding:10px;margin:3px;display: inline-block;flex-direction: row';
+            newDiv.style.backgroundColor=document.getElementById("arrcolor").value;
+            newDiv.style.height= ht;
+            newDiv.style.width=1/(num^2);
+document.getElementById("ArrayDisplay").appendChild(newDiv);
+            //var containerArr=window.document.getElementById("ArrayDisplay");
+            //containerArr.appendChild('newDiv');
+            
+        }
+    inputLHtag.innerHTML = array;
 }
 
-inputs.forEach(input => input.addEventListener('change', handleUpdate));
-inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+}
+//console.log(array);
+
+
+
+
+
